538d62de30b6e5c315b0c04e0b0fe8e177c83004
Anjali <anjalipndey06@gmail.com>
2025-11-27 14:32:00 +0530

    ProductFetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,16 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import NotFound from './pages/NotFound'
 import Homepage from './pages/Homepage'
+import PrivateRoute from './routes/PrivateRoute'
 const App = () => {
   return (
     <>
    <Routes>
-    <Route path = "/" element={<Navigate to = "/login"/>}/>
+    <Route path = "/" element={<Navigate to = "/home"/>}/>
     <Route path="/login" element={<Login/>}/>
-     <Route path="/home" element={<Homepage/>}/>
+     <Route path = "/home" element={<PrivateRoute></PrivateRoute>}>
+     <Route path = "/home" element={<Homepage/>}/>
+     </Route>
     <Route path="/signup" element={<Signup/>}/>
     <Route path="*" element={<NotFound/>}/>
    </Routes>
